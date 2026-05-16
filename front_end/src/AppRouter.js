62cf3d0d9ac874d8e904cb703f0a6e80b79d5619
FT1E <ft1hcfu@gmail.com>
2026-05-16 16:29:00 +0200

    Merge conflict

--- front_end/src/AppRouter.js
+++ front_end/src/AppRouter.js
@@ -1,29 +1,20 @@
 import { BrowserRouter, Route, Routes } from 'react-router'
 import OceanHomeScreen from './components/OceanScreen/OceanHomeScreen'
-<<<<<<< HEAD
-import FishDetails from './components/FishDetails'
-
-export default function AppRouter(props) {
-    return (
-        <BrowserRouter basename=''>
-            <Routes>
-                <Route path='/' element={<OceanHomeScreen />} />
-                <Route path='/fish/:slug' element={<FishDetails />} />
-=======
 import NorthOceanScreen from './components/screens/NorthOceanScreen'
 import ReefsScreen from './components/screens/ReefsScreen'
 import TrenchScreen from './components/screens/TrenchScreen'
+import FishDetails from './components/FishDetails'
 
 
 export default function AppRouter(props){
     return (
         <BrowserRouter basename=''>
             <Routes>
-              <Route path='/' element={<OceanHomeScreen />} />
-             <Route path='/north-ocean' element={<NorthOceanScreen />} />
-             <Route path='/reefs' element={<ReefsScreen />} />
-             <Route path='/trench' element={<TrenchScreen />} />
->>>>>>> 4ffc04cf1c250c88a92b8963b7337d8a1a179b2c
+                <Route path='/' element={<OceanHomeScreen />} />
+                <Route path='/north-ocean' element={<NorthOceanScreen />} />
+                <Route path='/reefs' element={<ReefsScreen />} />
+                <Route path='/trench' element={<TrenchScreen />} />
+                <Route path='/fish/:slug' element={<FishDetails />} />
             </Routes>
         </BrowserRouter>
     )
