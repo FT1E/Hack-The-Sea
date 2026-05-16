0118cae20f46bdbca2e0aeafc7135b459754f8f7
FT1E <ft1hcfu@gmail.com>
2026-05-16 19:59:26 +0200

    Changed env name if render uses that specific name

--- front_end/src/services/backend_api.js
+++ front_end/src/services/backend_api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 axios.defaults.withCredentials = true;  // to preserve session data between different api calls from front end
 const server_api = axios.create({
-    baseURL : process.env.REACT_APP_DEPLOY_URL
+    baseURL : process.env.REACT_APP_BACKEND
 })
 
 export default server_api
