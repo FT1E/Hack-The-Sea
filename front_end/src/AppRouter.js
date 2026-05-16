1e59ae7021c6943e9e5543ef83c7ecf39375e279
bel-n <nade.belovinova@gmail.com>
2026-05-16 16:07:37 +0200

    correct navigation between regions

--- front_end/src/AppRouter.js
+++ front_end/src/AppRouter.js
@@ -1,13 +1,18 @@
 import { BrowserRouter, Route, Routes } from 'react-router'
 import OceanHomeScreen from './components/OceanScreen/OceanHomeScreen'
-import FirstScreen from './components/screens/FirstScreen'
+import NorthOceanScreen from './components/screens/NorthOceanScreen'
+import ReefsScreen from './components/screens/ReefsScreen'
+import TrenchScreen from './components/screens/TrenchScreen'
+
 
 export default function AppRouter(props){
     return (
         <BrowserRouter basename=''>
             <Routes>
-                <Route path='/' element={<OceanHomeScreen />} />
-                <Route path="/first" element={<FirstScreen />} />
+              <Route path='/' element={<OceanHomeScreen />} />
+             <Route path='/north-ocean' element={<NorthOceanScreen />} />
+             <Route path='/reefs' element={<ReefsScreen />} />
+             <Route path='/trench' element={<TrenchScreen />} />
             </Routes>
         </BrowserRouter>
     )
