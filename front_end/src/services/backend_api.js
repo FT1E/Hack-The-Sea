0deed8aaa1625d686ea458816d9b89a3576224b9
FT1E <ft1hcfu@gmail.com>
2026-05-16 19:36:36 +0200

    Wrong null check for env var

--- front_end/src/services/backend_api.js
+++ front_end/src/services/backend_api.js
@@ -3,7 +3,9 @@ import axios from 'axios'
 
 axios.defaults.withCredentials = true;  // to preserve session data between different api calls from front end
 const server_api = axios.create({
-    baseURL : (process.env.render_deploy_url == null ?  'http://localhost:4000/' : process.env.render_deploy_url)
+    baseURL : (!process.env.render_deploy_url 
+    ?  'http://localhost:4000/' 
+    : process.env.render_deploy_url)
 })
 
 export default server_api
