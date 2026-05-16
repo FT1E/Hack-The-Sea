739c9a20665e312d4ab049229033536fe0985697
FT1E <ft1hcfu@gmail.com>
2026-05-16 19:45:21 +0200

    Changed env name to start with REACT_APP_DEPLOY_URL

--- front_end/src/services/backend_api.js
+++ front_end/src/services/backend_api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 axios.defaults.withCredentials = true;  // to preserve session data between different api calls from front end
 const server_api = axios.create({
-    baseURL : (!process.env.render_deploy_url 
+    baseURL : (!process.env.REACT_APP_DEPLOY_URL 
     ?  'http://localhost:4000/' 
     : process.env.render_deploy_url)
 })
