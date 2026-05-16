7d42621533d12cb7a6f58332f18f1e9b382f5a73
Simona Cholakova <colakovas@yahoo.com>
2026-05-16 17:28:24 +0200

    All fish details available

---
+++ front_end/src/components/assets/modelMap.js
@@ -0,0 +1,24 @@
+// src/components/assets/modelMap.js
+import blackeyedgrandmother from './FishModels/blackeyedgrandmother.glb';
+import commoncuttlefish from './FishModels/commoncuttlefish.glb';
+import conicaltunicate from './FishModels/conicaltunicate.glb';
+import crnik from './FishModels/crnik.glb';
+import flathead from './FishModels/flathead.glb';
+import gentlehuntress from './FishModels/gentlehuntress.glb';
+import modrak from './FishModels/modrak.glb';
+import pisanica from './FishModels/pisanica.glb';
+import stripedcroaker from './FishModels/stripedcroaker.glb';
+
+const modelMap = {
+  blackeyedgrandmother,
+  commoncuttlefish,
+  conicaltunicate,
+  crnik,
+  flathead,
+  gentlehuntress,
+  modrak,
+  pisanica,
+  stripedcroaker,
+};
+
+export default modelMap;
